dab1dc447a71ee0da8d62ef827920161ceae3994
Robin <robin@digi.ninja>
2021-12-08 11:55:53 +0000

    basic working server

---
+++ cracked_flask.py
@@ -0,0 +1,23 @@
+from flask import Flask, session
+from waitress import serve
+
+app = Flask(__name__)
+
+# This is very important, keep this really secret!
+app.config['SECRET_KEY'] = 'monkey'
+
+@app.route('/')
+def index():
+    session['hello'] = "world"
+    if not 'username' in session:
+        session['username'] = "robin"
+
+    if session['username'] == "admin":
+        ret_str = "Welcome back administrator"
+    else:
+        ret_str = "Welcome back "+ session["username"]
+        
+    return ret_str + "\n"
+
+if __name__ == "__main__":
+    serve(app, host="127.0.0.1", port=5000)
